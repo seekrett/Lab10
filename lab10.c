689c5cfc10babec97482b42b3c265b77b89e7dc0
Yuto <63772196+seekrett@users.noreply.github.com>
2025-04-02 13:44:09 -0400

    added template

--- lab10.c
+++ lab10.c
@@ -5,3 +5,20 @@ to swap two variables, coded in assembly language.
 
 // include statements
 #include <stdio.h>
+
+// swap function
+void swap(int *x, int *y) {
+    // code here
+}
+
+// swap function in assembly
+void swap_asm(int *x, int *y) {
+    // code here
+}
+
+
+//main function
+int main() {
+    // code here
+    return 0;
+}
